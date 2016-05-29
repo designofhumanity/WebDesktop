e4c27b01fdd9cb8a20a9dfc542cb48c1fd65ddb1
nickraziel <nickraziel@gmail.com>
2016-05-29 21:51:54 +0300

    shorcut*.js translation

--- js/shortcut2.js
+++ js/shortcut2.js
@@ -1,5 +1,5 @@
 ﻿//id,iconName,iconUrl,url,width,height
 var shortcut = [
-[0,"普通版","images/i/icon21.png","http://www.zi-han.net/",940,500],
-[1,"移动版","images/i/icon22.png","http://www.zi-han.net/m",1024,500]
+[0,"regular version","images/i/icon21.png","http://www.zi-han.net/",940,500],
+[1,"Mobile Edition","images/i/icon22.png","http://www.zi-han.net/m",1024,500]
     ]
